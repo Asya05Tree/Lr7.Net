d543036d2111b7128bbf7bc8cd3cb3b9d1e84987
Nasya_K <71969033+Asya05Tree@users.noreply.github.com>
2024-10-22 10:06:05 +0300

    Add files via upload

---
+++ Controllers/FileController.cs
@@ -0,0 +1,32 @@
+﻿using Lr7.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Lr7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(FileDownloadModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var content = $"{model.FirstName} {model.LastName}";
+                var bytes = Encoding.UTF8.GetBytes(content);
+
+                var fileName = model.FileName.EndsWith(".txt") ?
+                    model.FileName :
+                    $"{model.FileName}.txt";
+
+                return File(bytes, "text/plain", fileName);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/FileDownloadModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Lr7.Models
+{
+    public class FileDownloadModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FileName { get; set; }
+    }
+}
